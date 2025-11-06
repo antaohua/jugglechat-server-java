4987b1e1fb562a74cb38814db9211118605d4037
安韬华 <antaohua@icloud.com>
2025-11-06 14:48:30 +0800

    Define missing controllers and API models

--- src/main/java/com/juggle/chat/controllers/GroupManageController.java
+++ src/main/java/com/juggle/chat/controllers/GroupManageController.java
@@ -2,46 +2,65 @@ package com.juggle.chat.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juggle.chat.apimodels.GroupAdministratorsReq;
+import com.juggle.chat.apimodels.GroupManagement;
+import com.juggle.chat.apimodels.GroupOwnerChgReq;
 import com.juggle.chat.apimodels.Result;
+import com.juggle.chat.apimodels.SetGroupHisMsgVisibleReq;
+import com.juggle.chat.apimodels.SetGroupMemberMuteReq;
+import com.juggle.chat.apimodels.SetGroupMuteReq;
+import com.juggle.chat.apimodels.SetGroupVerifyTypeReq;
 
 @RestController
 @RequestMapping("/jim/groups/management")
 public class GroupManageController {
     @PostMapping("/chgowner")
-    public Result chgGroupOwner(){
+    public Result chgGroupOwner(@RequestBody GroupOwnerChgReq req){
         return new Result(0, "");
     }
 
     @PostMapping("/setmute")
-    public Result setGroupMute(){
+    public Result setGroupMute(@RequestBody SetGroupMuteReq req){
+        return new Result(0, "");
+    }
+
+    @PostMapping("/setgrpmembersmute")
+    public Result setGroupMembersMute(@RequestBody SetGroupMemberMuteReq req){
         return new Result(0, "");
     }
 
     @PostMapping("/setgrpverifytype")
-    public Result setGrpVerifyType(){
+    public Result setGrpVerifyType(@RequestBody SetGroupVerifyTypeReq req){
         return new Result(0, "");
     }
 
     @PostMapping("/sethismsgvisible")
-    public Result setGrpHisMsgVisible(){
+    public Result setGrpHisMsgVisible(@RequestBody SetGroupHisMsgVisibleReq req){
         return new Result(0, "");
     }
 
-    @PostMapping("/adminstrators/add")
-    public Result addGrpAdministrator(){
+    @PostMapping("/administrators/add")
+    public Result addGrpAdministrator(@RequestBody GroupAdministratorsReq req){
         return new Result(0, "");
     }
 
-    @PostMapping("/adminstrators/del")
-    public Result delGrpAdministrator(){
+    @PostMapping("/administrators/del")
+    public Result delGrpAdministrator(@RequestBody GroupAdministratorsReq req){
         return new Result(0, "");
     }
 
-    @GetMapping("/adminstrators/list")
-    public Result qryGrpAdministrator(){
+    @GetMapping("/administrators/list")
+    public Result qryGrpAdministrator(@RequestParam("group_id") String groupId){
+        return new Result(0, "");
+    }
+
+    @PostMapping("/set")
+    public Result setGrpManagementConfs(@RequestBody GroupManagement management){
         return new Result(0, "");
     }
 }
