567c34bcdbbf4e7293945563e8a5f3231640171c
antaohua <antaohua@icloud.com>
2025-11-19 19:06:56 +0800

    add update

--- src/main/java/com/juggle/chat/file/MinioStorage.java
+++ src/main/java/com/juggle/chat/file/MinioStorage.java
@@ -16,27 +16,28 @@ import io.minio.errors.XmlParserException;
 import io.minio.http.Method;
 
 public class MinioStorage {
-    private final MinioClient client;
-    private final String bucket;
+//    private final MinioClient client;
+//    private final String bucket;
 
     public MinioStorage(MinioConfig config) {
-        this.client = MinioClient.builder()
-                .endpoint(config.getEndpoint())
-                .credentials(config.getAccessKey(), config.getSecretKey())
-                .build();
-        this.bucket = config.getBucket();
+//        this.client = MinioClient.builder()
+//                .endpoint(config.getEndpoint())
+//                .credentials(config.getAccessKey(), config.getSecretKey())
+//                .build();
+//        this.bucket = config.getBucket();
     }
 
     public String preSignedPutUrl(String objectKey)
             throws ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException,
             ServerException, XmlParserException, IOException, NoSuchAlgorithmException, InvalidKeyException {
-        GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs.builder()
-                .bucket(bucket)
-                .object(objectKey)
-                .method(Method.PUT)
-                .expiry(15, TimeUnit.MINUTES)
-                .build();
-        return client.getPresignedObjectUrl(args);
+//        GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs.builder()
+//                .bucket(bucket)
+//                .object(objectKey)
+//                .method(Method.PUT)
+//                .expiry(15, TimeUnit.MINUTES)
+//                .build();
+//        return client.getPresignedObjectUrl(args);
+        return null;
     }
 
 }
